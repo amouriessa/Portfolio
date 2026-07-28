1f22d081e9420b289e222740872e3b6fa3998a9a
amouriessa <bagassarasbp@gmail.com>
2026-07-28 17:30:03 +0700

    feat(navbar): remove home tab

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -188,7 +188,6 @@ export default function Navbar() {
               />
 
               {[
-                "home",
                 "abouts",
                 "experience",
                 "projects",
